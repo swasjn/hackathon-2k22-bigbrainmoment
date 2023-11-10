d3b60734a96f18d708d16830d411e5a2bda65afd
swastijn <zcabain@ucl.ac.uk>
2023-11-10 01:08:22 +0000

    randomPlayer trial

---
+++ UCL CS/Design and Professional Skills/ENGF34-2023-main/assignments/tetris-master/player.py
@@ -0,0 +1,46 @@
+from board import Direction, Rotation, Action
+from random import Random
+import time
+
+class Player:
+    def choose_action(self, board):
+        raise NotImplementedError
+
+
+class RandomPlayer(Player):
+    def __init__(self, seed=None):
+        self.random = Random(seed)
+
+    def print_board(self, board):
+        print("--------")
+        for y in range(24):
+            s = ""
+            for x in range(10):
+                if (x,y) in board.cells:
+                    s += "#"
+                else:
+                    s += "."
+            print(s, y)
+
+    def choose_action(self, board):
+        self.print_board(board)
+        time.sleep(0.5)
+        
+        if self.random.random() > 0.97:
+            # 3% chance we'll discard or drop a bomb
+            return self.random.choice([
+                Action.Discard,
+                Action.Bomb,
+            ])
+        else:
+            # 97% chance we'll make a normal move
+            return self.random.choice([
+                Direction.Left,
+                Direction.Right,
+                Direction.Down,
+                Rotation.Anticlockwise,
+                Rotation.Clockwise,
+            ])
+
+
+SelectedPlayer = RandomPlayer
